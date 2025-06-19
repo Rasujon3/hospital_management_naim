7649216bebbd63cc2e91f1196af093c0f2e05229
Rasujon3 <rasujon3@gmail.com>
2025-06-20 01:51:36 +0600

    first commit

---
+++ app/Models/AppointmentChange.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AppointmentChange extends Model
+{
+    use HasFactory;
+}
